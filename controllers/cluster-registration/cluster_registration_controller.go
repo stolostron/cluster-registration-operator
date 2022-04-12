67f959e10a68b12dd139dcc963943969379fe023
Robin Y Bobbitt <rbobbitt@redhat.com>
2022-04-11 21:15:22 -0400

    base64 encode certificate-authority-data

--- controllers/cluster-registration/cluster_registration_controller.go
+++ controllers/cluster-registration/cluster_registration_controller.go
@@ -13,6 +13,8 @@ import (
 	"github.com/go-logr/logr"
 	giterrors "github.com/pkg/errors"
 
+	b64 "encoding/base64"
+
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
@@ -328,8 +330,9 @@ func (r *RegisteredClusterReconciler) syncManagedClusterKubeconfig(regCluster *s
 		Build()
 
 	files := []string{
-		"cluster-registration/kubeconfig-secret.yaml",
+		"cluster-registration/kubeconfig_secret.yaml",
 	}
+
 	secretName := fmt.Sprintf("%s-cluster-secret", regCluster.Name)
 	values := struct {
 		ApiURL      string
@@ -341,7 +344,7 @@ func (r *RegisteredClusterReconciler) syncManagedClusterKubeconfig(regCluster *s
 	}{
 		ApiURL:      apiUrl,
 		Token:       string(token.Data["token"]),
-		CABundle:    string(token.Data["ca.crt"]),
+		CABundle:    b64.StdEncoding.EncodeToString(token.Data["ca.crt"]),
 		SecretName:  secretName,
 		ClusterName: regCluster.Name,
 		Namespace:   regCluster.Namespace,
